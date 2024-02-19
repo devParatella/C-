8e7952dc205639259e9f12fc3fc0e91b9ef187ca
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:30:08 -0300

    Add files via upload

---
+++ Arrays/Array_Unidimencional_Vetor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ArrayUnidimencionalVetor{
+    static void Main(){
+        //forma extença
+        int[] n1=new int[5];
+
+        n1[0] = 1;
+        n1[1] = 2;
+        n1[2] = 3;
+        n1[3] = 4;
+        n1[4] = 5;
+
+        Console.WriteLine(n1[0]);
+
+        //Atribuindo valores diretamente
+        int[] n2=new int[5]{1,2,3,4,5};
+        Console.WriteLine(n2[2]);
+
+        //Forma simplificada
+        int[] n3=[1, 2, 3, 4, 5];
+        Console.WriteLine(n3[3]);
+
+        //String
+        string[] veiculos=["Carro","Avião","Navio"];
+        Console.WriteLine(veiculos[2]);
+
+
+    }
+}
