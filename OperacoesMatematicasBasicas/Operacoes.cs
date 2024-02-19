b7f8cbeea5023b6af4da56d537ee789c65dff854
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:33:12 -0300

    Add files via upload

---
+++ OperacoesMatematicasBasicas/Operacoes.cs
@@ -0,0 +1,18 @@
+﻿//Executar operações matemáticas básicas
+int soma = 7 + 5;
+int diferenca = 7 - 5;
+int producto = 7 * 5;
+int intQuociente = 7 /5;
+float floatQuociente = 7 / 5f;
+float floatInvQuociente = 7f / 5;
+double doubleQuociente = 7 / 5.0;
+decimal decimalQuociente = 7.00m / 5;
+
+Console.WriteLine($"Soma: int soma = 7 + 5; {soma}");
+Console.WriteLine($"Diferença: int diferenca = 7 - 5; {diferenca}");
+Console.WriteLine($"Produto:  int producto = 7 * 5; {producto}");
+Console.WriteLine($"Quociente de 7 / 5 usando int: int intQuociente = 7 /5; {intQuociente}");
+Console.WriteLine($"Quociente de 7 / 5 usado float: float floatQuociente = 7 / 5f; {floatQuociente}");
+Console.WriteLine($"Quociente de 7 / 5 usado float invertida: float floatInvQuociente = 7f / 5; {floatInvQuociente}");
+Console.WriteLine($"Quocinete de 7 / 5 usando double: double doubleQuociente = 7 / 5; {doubleQuociente}");
+Console.WriteLine($"Quocinete de 7 / 5 usando decimal: decimal decimalQuociente = 7.00m / 5; {decimalQuociente}");
