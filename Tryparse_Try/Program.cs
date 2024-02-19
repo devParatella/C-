afc7a99ff89f3e4ee7ea886dda860e2d8d6fd340
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:35:41 -0300

    Add files via upload

---
+++ Tryparse_Try/Program.cs
@@ -0,0 +1,14 @@
+﻿string numeroString = "abc";
+int numero;
+
+if (int.TryParse(numeroString, out numero))
+{
+    // Código a ser executado se a conversão for bem-sucedida
+    Console.WriteLine("Conversão bem-sucedida: " + numero);
+}
+else
+{
+    // Código a ser executado se a conversão falhar
+    Console.WriteLine("Falha na conversão. Tratando de forma específica.");
+}
+bool _ = int.TryParse(numeroString, out numero);
