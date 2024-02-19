8c2a4272499f819202ab18066e491e05341dcee8
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:29:43 -0300

    Add files via upload

---
+++ adicao/Adicao.cs
@@ -0,0 +1,16 @@
+﻿//Adição com conversão implicita
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+//Concatenação
+string firstName = "Bob";
+int widgetSold = 7;
+Console.WriteLine(firstName + " sold " + widgetSold + " widgets.");
+//Bob sold 7 widgets.
+Console.WriteLine(firstName + " sold " + widgetSold + 7 + " widgets.");
+//Bob sold 77 widgets.
+
+//Adição explicita 
+Console.WriteLine(firstName + " sold " + (widgetSold + 7) + " widgets.");
+//Bob sold 14 widgets.
