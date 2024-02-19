87b92fac85822ec74094945a559665efb934d506
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:31:22 -0300

    Add files via upload

---
+++ funcoes/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Função - Sintaxe de uma função:
+//  tipoRetorno NomeDaFuncao(parâmetros)
+//  {
+//       // Corpo da função
+//       // Código que realiza a tarefa
+//       return resultado; // Se houver um tipo de retorno
+//  }
+
+static int calculoSoma(int valorA, int valorB)
+{
+    return valorA + valorB;
+}
+
+int resultado = calculoSoma(5, 3);
+
+Console.WriteLine("O resultado da soma é: " + resultado);
