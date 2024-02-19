497e10ed53d0aaec12c2bcfae3d5e3de9e92961c
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:31:39 -0300

    Add files via upload

---
+++ Generics/ClassHelper/ClassHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Generics
+{
+    public static class ClassHelper<T> where T : class
+    {
+        public static string ObterNomeClasse() =>
+            typeof(T).Name;
+
+        public static MethodInfo ObterMetodoPorNome(string nome) =>
+            typeof(T)
+            .GetMethods()
+            .FirstOrDefault(p => p.Name.Equals(nome, StringComparison.Ordinal))!;
+    }
+}
---
+++ Generics/ClassHelper/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO.Pipes;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaBase pessoa = new Pessoa(1, "João", 19);
+            var metodo = ClassHelper<Pessoa>.ObterMetodoPorNome("pOSSUImAIORiDADE");
+
+            var maiorIdade = metodo.Invoke(pessoa, null);
+            Console.WriteLine($"Possui maior idade: {maiorIdade}");
+        }
+    }
+
+    public abstract class PessoaBase {   }
+
+    public class Pessoa : PessoaBase
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        protected int Idade { get; set; }
+
+        public Pessoa( int id, string nome, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public bool PossuiMaiorIdade() =>
+            Idade >=18;
+    }
+}
