d203ad883857e3784607eaa04963eccff723a659
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:32:23 -0300

    Add files via upload

---
+++ Iteradores/Do_While/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int dayOfWeek = 1; // Inicializando com o primeiro dia da semana (Domingo)
+
+        do
+        {
+            Console.WriteLine(GetDayOfWeekName(dayOfWeek));
+            dayOfWeek++;
+
+            // Se atingir o último dia da semana (Sábado), reinicia no primeiro dia (Domingo)
+            if (dayOfWeek > 7)
+                dayOfWeek = 1;
+
+        } while (dayOfWeek != 1); // Continua enquanto não voltar ao Domingo
+    }
+
+    static string GetDayOfWeekName(int dayOfWeek)
+    {
+        switch (dayOfWeek)
+        {
+            case 1:
+                return "Domingo";
+            case 2:
+                return "Segunda-feira";
+            case 3:
+                return "Terça-feira";
+            case 4:
+                return "Quarta-feira";
+            case 5:
+                return "Quinta-feira";
+            case 6:
+                return "Sexta-feira";
+            case 7:
+                return "Sábado";
+            default:
+                return "Dia inválido";
+        }
+    }
+}
