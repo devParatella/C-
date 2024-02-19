5f216e0fdae06b143322411707a9acb7b0d84aee
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-19 13:32:42 -0300

    Add files via upload

---
+++ MS_Learn_030224/MS_Learn_030224.cs
@@ -0,0 +1,17 @@
+﻿
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //criando uma instância de classe 
+        //Random dice = new Random();
+
+        chamando o método Next
+        int roll = dice.Next(1, 2);
+        Console.WriteLine(roll);
+        //Console.WriteLine(Random.Next(1, 999999999));//funcionou assim tb.
+        //Console.WriteLine(dice.next(1, 9999999999));
+        //dice.next(1, 9999999999)//ignorar o valor de retorno seria inútil. 
+    }
+}
